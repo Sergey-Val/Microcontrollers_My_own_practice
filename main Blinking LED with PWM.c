ce7cad3537418765e0a328112ae059467cf208ef
Sergey <Sergey.Val.Ln@yandex.ru>
2024-12-20 12:39:17 +0300

    Add files via upload

---
+++ main Blinking LED with PWM.c
@@ -0,0 +1,52 @@
+﻿/*
+ * Smoothly blinking LED.c
+ *
+ * Created: 30.11.2024 12:45:45
+ * Author : Сергей
+ */ 
+
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+void PWM_settings(void)
+{
+  TCCR1A |= (1<<COM1A1); //Non-inverting PWM
+  
+  TCCR1A |= (1<<WGM11) | (1<<WGM10); //10-bit PWM. Phase correct
+
+  TCCR1B |= (1<<CS11); //Prescaler 8
+  OCR1A = 0; //Initial state of the output compare register
+}
+
+int main(void)
+{
+    DDRB |= (1<<PB1); //Making PB1 output for PWM;
+	DDRC |= (1<<PC5);
+
+	PORTC |= (1<<PC5);
+	PORTB = 0x00; //All pins are off as default
+	PWM_settings();
+	unsigned char it = 0;
+    while (1) 
+    {
+	  if (it == 0)
+	  {
+	    while (OCR1A <= 511)
+		{
+		  OCR1A++;
+		  _delay_ms(5);
+		}
+		it = 1;
+	  }
+	  if (it == 1)
+	  {
+	    while (OCR1A >= 1)
+		{
+		  OCR1A--;
+		  _delay_ms(5);
+		}
+		it = 0;
+	  }
+    }
+ }
---
+++ main Electronic protector.c
@@ -0,0 +1,47 @@
+﻿/*
+ * Electronic protector.c
+ *
+ * Created: 30.05.2024 14:18:39
+ * Author : Сергей
+ */ 
+
+#define F_CPU 1000000UL
+#include <avr/io.h>
+
+void ADC_settings(void)
+{
+   ADCSRA |= (1<<ADEN); //Enable the ADC
+   ADCSRA |= (1<<ADFR); //Set Free Running mode
+
+   //Division factor for the CPU is 8. Frequency of discretization 125 kHz.
+   ADCSRA &= ~(1<<ADPS2);
+   ADCSRA |= (1<<ADPS1) | (1<<ADPS0);
+
+   ADMUX |= (1<<REFS1) | (1<<REFS0); //Internal 2.56V voltage reference with external capacitor at AREF pin
+   ADMUX &= ~(1<<ADLAR); //The result of ADC is RIGHT adjusted
+   ADMUX |= (1<<MUX0); //Enable ADC1
+}
+
+int main(void)
+{
+    DDRB |= (1<<1) | (1<<0);
+	PORTB &= ~((1<<1) | (1<<0));
+
+	ADC_settings();
+	ADCSRA |= (1<<ADSC); //Start conversion
+
+    while (1) 
+    {
+	  if (ADCSRA & (1<<ADIF))
+	  {
+	    if (ADC < 800) //Voltage is lower than 1.93V. When choosing voltage higher than 2.56V, it's considered infinite, and the condition becomes true.
+		{
+		  PORTB |= (1<<0) | (1<<1);
+		}
+		else PORTB &= ~((1<<0) | (1<<1));
+
+		ADCSRA |= (1<<ADIF);
+	  }
+    }
+}
+
